450eb3f84a44c7ec6c19ca81d82ecba9833bdb97
sachinGurjar123 <sachin.kumar@deorwine.com>
2023-01-25 16:14:42 +0530

    massage send error fix

--- src/ChatUi.js
+++ src/ChatUi.js
@@ -21,10 +21,13 @@ const ChatUi = () => {
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjNiYmQ1ZWY3NDVhNmNmNTE5ZWE3ZDQyIiwiaWF0IjoxNjc0MTMxNzU1LCJleHAiOjE2NzY3MjM3NTV9.4-kTHPyMq1DYGsBgm2fGnOETVyCKpAUS5-DKWqyiGlU";
   const fetchData = () => {
-    fetch("http://172.105.41.247:5004/api/v1/chats?page_no=1&limit=10", {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-    })
+    fetch(
+      "http://172.105.41.247:5004/api/v1/chats?page_no=1&limit=10&user_id=63bbd5ef745a6cf519ea7d42",
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    )
       .then((res) => res.json())
       .then((result) => {
         setData(result.result.data);
@@ -36,8 +39,8 @@ const ChatUi = () => {
   }, []);
 
   const fetchHistory = (id) => {
-    setUserId(id);
-    // console.log(id, "kidddddddddd");
+    // setUserId(id);
+    console.log(id, "kidddddddddd");
     const token =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjNiZTRkMjE4MGRkMjFiMjM4ZTdjMjRmIiwiaWF0IjoxNjczODc0MjU3LCJleHAiOjE2NzY0NjYyNTd9.YHZjuuIiWINgNbk94YNO5nbl0vjc3a6sXIxsGGuBgHA";
     fetch(
@@ -49,7 +52,8 @@ const ChatUi = () => {
     )
       .then((res) => res.json())
       .then((result) => {
-        setMas(result.result.data);
+        setMas(result.result.data.reverse());
+
         // console.log(result.result.data, "text messages_____--------__----");
       });
   };
@@ -60,15 +64,16 @@ const ChatUi = () => {
     data.append("to_send", userId);
     data.append("message", message);
     data.append("is_file", false);
-    data.append("send_to_role", "vendor");
+    // data.append("send_to_role", "vendor");
     const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjNiZTRkMjE4MGRkMjFiMjM4ZTdjMjRmIiwiaWF0IjoxNjczODc0MjU3LCJleHAiOjE2NzY0NjYyNTd9.YHZjuuIiWINgNbk94YNO5nbl0vjc3a6sXIxsGGuBgHA";
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjNiYmQ1ZWY3NDVhNmNmNTE5ZWE3ZDQyIiwiaWF0IjoxNjc0NjI2OTE3LCJleHAiOjE2NzcyMTg5MTd9.MZ6406QOHM2HBCGbAGX9-y0h-U4cdC9fuosFUw_NSfA";
     fetch("http://172.105.41.247:5004/api/v1/chats/send_message", {
       method: "POST",
       headers: { Authorization: `Bearer ${token}` },
       body: data,
     });
   };
+
   return (
     <div>
       <Container>
@@ -78,18 +83,23 @@ const ChatUi = () => {
               <>
                 <UserBox
                   onClick={() => {
-                    fetchHistory(elm.members[1]);
-                    // setUserId(elm.members[0]);
-                    console.log(elm._id, "RoomUser id");
+                    fetchHistory(elm._id);
+                    setUserId(
+                      elm.members[0] === "63bbd5ef745a6cf519ea7d42"
+                        ? elm.members[1]
+                        : elm.members[0]
+                    );
+                    setUserId(elm.member_users[1]._id);
+                    // console.log(elm.members[0], "RoomUser id");
                   }}
                 >
                   <LogoBtn>
-                    {elm.member_users[0].first_name.match(/\b(\w)/g)}
+                    {elm.member_users[1]?.first_name.match(/\b(\w)/g)}
                   </LogoBtn>
                   <NameDiv>
                     <p>
                       <span style={{ fontSize: "20px" }}>
-                        {elm.member_users[0].first_name}
+                        {elm.member_users[1].first_name}
                       </span>
                       <br />
                       {elm.last_message.message}
